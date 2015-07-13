2882fcb5ec92c103e28a1ff2f3d0d42d441e61e3
sPaz <kumar.pandya91@gmail.com>
2015-07-12 22:47:58 -0400

    implemented days_before, days_after and between methods for WorkCalendar module

--- lib/workcalendar.rb
+++ lib/workcalendar.rb
@@ -8,9 +8,6 @@ module WorkCalendar
 		# Gets and sets the configuration for WorkCalendar module
 		attr_accessor :configuration
 
-		# Initially set the configuration to new configuration so user can use the gem right away
-		configuration = Configuration.new
-
 		# Return true if date is active, false otherwise
 		# 
 		# +date+ - Date to check for
@@ -18,6 +15,56 @@ module WorkCalendar
 		def active?(date)
 			# Date is active if it's a weekday and it's not a holiday
 			date.is_weekday? && !date.is_holiday?
+		end
+
+		# Returns nth active date before the current date
+		# 
+		# +n+ - number of dates to go back till
+		# +date+ - Date to go back on
+		# 
+		def days_before(n, date)
+			loop do
+				# Set date to previous active date
+				date = date.get_active_date(:-)
+				n -= 1
+				# Found the date when end of counter
+				break if n == 0
+			end
+			date
+		end
+
+		# Returns nth active date after the current date
+		# 
+		# +n+ - Number of dates to go forward till
+		# +date+ - Date to go forward on
+		# 
+		def days_after(n, date)
+			loop do
+				# Set date to next active date
+				date = date.get_active_date
+				n -= 1
+				# Found the date when end of counter
+				break if n == 0
+			end
+			date
+		end
+
+		# Returns an array of all active dates beween two given dates
+		# 
+		# +date1+ - Starting date range
+		# +date2+ - Ending date range
+		# 
+		def between(date1, date2)
+			# If 
+			raise "Starting date ends before ending date" if date1 > date2
+			cur_date = date1
+			res = Array.new
+			while cur_date < date2
+				res << cur_date
+				# We can also use *days_after(1, cur_date)* to get next date
+				cur_date = cur_date.get_active_date
+			end
+			res
 		end
 
 		# 
